721fd8fe4e556db2043bb4995f3195c6ca132ebf
Valentin <guillaumevalentin76@gmail.com>
2024-05-24 17:28:42 +0200

    Correction de l'erreur de récupération de variable d'environnement cloudinary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadPicture = (picture, folder = null, needConversion = true) => {
