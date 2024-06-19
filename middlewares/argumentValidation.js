e26be4580a30d61da18a03f2d213de28de379f30
Valentin <guillaumevalentin76@gmail.com>
2024-06-19 02:11:42 +0200

    log

--- middlewares/argumentValidation.js
+++ middlewares/argumentValidation.js
@@ -33,7 +33,7 @@ const isArgumentValid = ({
 }) => {
   const isString = (req, res, next) => {
     try {
-      log(req);
+      console.log(req);
       let argument = req[parameterType][argumentName];
 
       console.log(argument);
